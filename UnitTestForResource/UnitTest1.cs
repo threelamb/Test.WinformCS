cca3a78faafd9fc95cba3778e894e677545791dd
llyang@Ctrip.com <llyang@Ctrip.com>
2015-11-17 21:06:57 +0800

    Only CurrentUICulture reflect the Resource.Culture when Resource.Culture == null.

--- UnitTestForResource/UnitTest1.cs
+++ UnitTestForResource/UnitTest1.cs
@@ -10,28 +10,19 @@ namespace UnitTestForResource
         [TestMethod]
         public void Test_ResourceAndThreadCulture()
         {
-            var a = Resource.Resource.Culture;
+            //Only CurrentUICulture reflect the Resource.Culture when Resource.Culture == null.
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US"); 
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
             var name = Resource.Resource.lbl_name;
-            var b = Thread.CurrentThread.CurrentCulture;
-            var c = Thread.CurrentThread.CurrentUICulture;
-            Resource.Resource.Culture = new System.Globalization.CultureInfo("en-US");
-            var name1 = Resource.Resource.lbl_name;
-            var d = Resource.Resource.Culture;
-            
             new Thread(new ThreadStart(target)).Start();
         }
 
         void target()
         {
-            var a = Resource.Resource.Culture;
-            var name = Resource.Resource.lbl_name;
-            var b = Thread.CurrentThread.CurrentCulture;
-            var c = Thread.CurrentThread.CurrentUICulture;
-            Resource.Resource.Culture = new System.Globalization.CultureInfo("zh-CN");
-            var name1 = Resource.Resource.lbl_name;
-            var d = Resource.Resource.Culture;
             Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
             Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+            var name = Resource.Resource.lbl_name;
+            new Thread(new ThreadStart(target)).Start();
         }
     }
 }
